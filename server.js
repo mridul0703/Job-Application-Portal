f893cbafd04b77c123114450b3d0fcc6d75d1d5e
Mridul M Kumar <98074317+mridul0703@users.noreply.github.com>
2025-04-17 13:21:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "*",
+  origin: "https://job-application-portal-wrao.onrender.com",
   credentials: true,
 }));
 app.use(express.json());
